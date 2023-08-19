72326d786f00f87bf6e8a5ac6c39012332f9869c
Lianny Coringa <lianny_costa@hotmail.com>
2023-08-19 15:57:01 -0300

    corrigindo erro de lint

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from 'react';
-import useLocalStorage from '../hooks/useLocalStorage';
 import { useNavigate } from 'react-router-dom';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function Login() {
   const [disabled, setDisabled] = useState(true);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { valueStorage, setValue } = useLocalStorage('user', { [email]: email });
+  const { setValue } = useLocalStorage('user', { email });
   const navigate = useNavigate();
 
   useEffect(() => {
     const emailRegex = /\S+@\S+\.\S+/;
     const validEmail = emailRegex.test(email);
     const IsValidPassword = password.length > 6;
-    const valid = !validEmail || !IsValidPassword ? setDisabled(true) : setDisabled(false);
+    return !validEmail || !IsValidPassword ? setDisabled(true) : setDisabled(false);
   }, [email, password]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setValue({ [email]: email });
+    setValue({ email });
     setDisabled(true);
     navigate('/meals');
   };
