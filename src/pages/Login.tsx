60d81b94ce6625f760f083aa5583439e072e8479
Lianny Coringa <lianny_costa@hotmail.com>
2023-08-19 12:57:00 -0300

    Implementando a validacao do email e da senha do login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from 'react';
+
 function Login() {
+  const [disabled, setDisabled] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    const validEmail = emailRegex.test(email);
+    const IsValidPassword = password.length > 6;
+    const valid = validEmail && IsValidPassword ? setDisabled(true) : setDisabled(false);
+  }, [email, password]);
+
+  const handleSubmit = () => {
+    setDisabled(true);
+  };
+
   return (
-    <form>
-      <input type="text" data-testid="email-input" />
-      <input type="text" data-testid="password-input" />
-      <button data-testid="login-submit-btn">Enter</button>
+    <form onSubmit={ handleSubmit }>
+      <input
+        type="text"
+        data-testid="email-input"
+        name="email"
+        value={ email }
+        onChange={ (event) => setEmail(event.target.value) }
+      />
+      <input
+        type="text"
+        data-testid="password-input"
+        name="password"
+        value={ password }
+        onChange={ (event) => setPassword(event.target.value) }
+      />
+      { disabled
+        ? <button data-testid="login-submit-btn">Enter</button>
+        : <button data-testid="login-submit-btn" disabled>Enter</button> }
     </form>
   );
 }
