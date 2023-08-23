693d01d31ced7f72c481ae8b08938644394a1225
Lianny Coringa <lianny_costa@hotmail.com>
2023-08-23 14:40:02 -0300

    Testes do header

--- src/context/RecipesProvider.tsx
+++ src/context/RecipesProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import RecipesContext from './RecipesContext';
 import { DataType } from '../types';
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -27,5 +27,83 @@ describe('Testa a Tela de Login', () => {
     await userEvent.type(email, 'test@test');
     await userEvent.type(password, '123456');
     expect(button).toBeDisabled();
+  });
+});
+
+describe('Testa o Header', () => {
+  test('Testa o Header do Meals', () => {
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
+    const title = screen.getByRole('heading', { name: 'Meals' });
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const searchIcon = screen.getByTestId('search-top-btn');
+
+    expect(title).toBeInTheDocument();
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).toBeInTheDocument();
+  });
+  test('Testa o Header do Drinks', () => {
+    renderWithRouter(<App />, { initialEntries: ['/drinks'] });
+    const title = screen.getByRole('heading', { name: 'Drinks' });
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const searchIcon = screen.getByTestId('search-top-btn');
+
+    expect(title).toBeInTheDocument();
+    expect(profileIcon).toBeInTheDocument();
+    expect(searchIcon).toBeInTheDocument();
+  });
+  test('Testa o Header do DoneRecipes', () => {
+    renderWithRouter(<App />, { initialEntries: ['/done-recipes'] });
+    const title = screen.getByRole('heading', { name: 'Done Recipes' });
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const buttons = screen.getAllByRole('button');
+
+    expect(title).toBeInTheDocument();
+    expect(profileIcon).toBeInTheDocument();
+    expect(buttons).toHaveLength(1);
+  });
+  test('Testa o Header do FavoriteRecipes', () => {
+    renderWithRouter(<App />, { initialEntries: ['/favorite-recipes'] });
+    const title = screen.getByRole('heading', { name: 'Favorite Recipes' });
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const buttons = screen.getAllByRole('button');
+
+    expect(title).toBeInTheDocument();
+    expect(profileIcon).toBeInTheDocument();
+    expect(buttons).toHaveLength(1);
+  });
+  test('Testa o Header do Profile', () => {
+    renderWithRouter(<App />, { initialEntries: ['/profile'] });
+    const title = screen.getByRole('heading', { name: 'Profile' });
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const buttons = screen.getAllByRole('button');
+
+    expect(title).toBeInTheDocument();
+    expect(profileIcon).toBeInTheDocument();
+    expect(buttons).toHaveLength(1);
+  });
+  test('Testa a barra de busca do header', async () => {
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
+    const searchIcon = screen.getByTestId('search-top-btn');
+
+    await userEvent.click(searchIcon);
+    const searchInput = screen.getByTestId('search-input');
+    const ingredientRadio = screen.getByTestId('ingredient-search-radio');
+    const nameRadio = screen.getByTestId('name-search-radio');
+    const firstLetterRadio = screen.getByTestId('first-letter-search-radio');
+    const buttonSearch = screen.getByTestId('exec-search-btn');
+
+    expect(searchInput).toBeInTheDocument();
+    expect(ingredientRadio).toBeInTheDocument();
+    expect(nameRadio).toBeInTheDocument();
+    expect(firstLetterRadio).toBeInTheDocument();
+    expect(buttonSearch).toBeInTheDocument();
+  });
+  test('Testa se ao clicar no icone de perfil, navega ate a profile', async () => {
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
+    const profileIcon = screen.getByTestId('profile-top-btn');
+
+    await userEvent.click(profileIcon);
+    expect(screen.getByText('Profile')).toBeInTheDocument();
   })
 });
+
