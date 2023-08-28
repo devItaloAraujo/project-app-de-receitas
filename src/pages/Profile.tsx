25031c2174963994af53e25c70645d6fd5b71945
Lianny Coringa <lianny_costa@hotmail.com>
2023-08-28 19:39:28 -0300

    Implementando o email pegando do localStorage e os buttons

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,9 +2,16 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user') as string);
   return (
     <>
       <Header title="Profile" perfil pesquisa={ false } />
+      <h3 data-testid="profile-email">{ email }</h3>
+      <div>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer />
     </>
 
