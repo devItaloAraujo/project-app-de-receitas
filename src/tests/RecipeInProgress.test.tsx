f73efef49e85c880e9ba77642b0c486189850d71
Luana Eugênia <luana.san2015.ls@gmail.com>
2023-08-29 02:11:49 -0300

    lint fix

--- src/tests/RecipeInProgress.test.tsx
+++ src/tests/RecipeInProgress.test.tsx
@@ -7,21 +7,23 @@ import { mealsResponse } from './mock/mealsById.mock';
 import { renderWithRouter } from './renderWithRouter';
 
 let render: RenderResult;
+const recipeInProgressPath = '/meals/52772/in-progress';
+const favoriteBtnId = 'favorite-btn';
+const whiteHeartIconPath = '/src/images/whiteHeartIcon.svg';
 
 beforeEach(async () => {
   global.fetch = vi.fn().mockResolvedValue({
     json: async () => (mealsResponse),
   });
   await act(() => {
-    render = renderWithRouter(<App />, { initialEntries: ['/meals/52772/in-progress'] });
-  })
+    render = renderWithRouter(<App />, { initialEntries: [recipeInProgressPath] });
+  });
   Object.assign(window.navigator, {
     clipboard: {
       writeText: vi.fn().mockImplementation(() => Promise.resolve()),
     },
   });
 });
-''
 
 afterEach(() => {
   vi.clearAllMocks();
@@ -32,40 +34,40 @@ describe('Testes tela de receitas em progresso', async () => {
   test('Deve mostrar o texto \'Link copied!\' ao clicar no botão \'share\'', async () => {
     const shareBtn = screen.getByTestId('share-btn');
     await userEvent.click(shareBtn);
-    
+
     await act(() => waitFor(() => expect(screen.getByText('Link copied!')).toBeInTheDocument()));
   });
 
   test('Deve marcar uma receita como favorita', async () => {
-    const favoriteBtnParent = screen.getByTestId('favorite-btn').parentElement as Element;
-    expect(favoriteBtnParent.children.item(0)?.getAttribute('src')).toEqual('/src/images/whiteHeartIcon.svg');
+    const favoriteBtnParent = screen.getByTestId(favoriteBtnId).parentElement as Element;
+    expect(favoriteBtnParent.children.item(0)?.getAttribute('src')).toEqual(whiteHeartIconPath);
     await userEvent.click(favoriteBtnParent);
     expect(favoriteBtnParent.children.item(0)?.getAttribute('src')).toEqual('/src/images/blackHeartIcon.svg');
   });
 
   test('Deve manter as receitas favoritas salvas', async () => {
-    const favoriteBtnParent = screen.getByTestId('favorite-btn').parentElement as Element;
-    expect(favoriteBtnParent.children.item(0)?.getAttribute('src')).toEqual('/src/images/whiteHeartIcon.svg');
+    const favoriteBtnParent = screen.getByTestId(favoriteBtnId).parentElement as Element;
+    expect(favoriteBtnParent.children.item(0)?.getAttribute('src')).toEqual(whiteHeartIconPath);
     await userEvent.click(favoriteBtnParent);
 
     render.unmount();
-    await act(() => renderWithRouter(<App />, { initialEntries: ['/meals/52772/in-progress'] }));
+    await act(() => renderWithRouter(<App />, { initialEntries: [recipeInProgressPath] }));
 
-    let favoriteBtn = screen.getByTestId('favorite-btn');
+    const favoriteBtn = screen.getByTestId(favoriteBtnId);
     expect(favoriteBtn.getAttribute('src')).toEqual('/src/images/blackHeartIcon.svg');
   });
 
   test('Deve remover as receitas favoritas', async () => {
-    const favoriteBtnParent = screen.getByTestId('favorite-btn').parentElement as Element;
-    expect(favoriteBtnParent.children.item(0)?.getAttribute('src')).toEqual('/src/images/whiteHeartIcon.svg');
+    const favoriteBtnParent = screen.getByTestId(favoriteBtnId).parentElement as Element;
+    expect(favoriteBtnParent.children.item(0)?.getAttribute('src')).toEqual(whiteHeartIconPath);
     await userEvent.click(favoriteBtnParent);
     await userEvent.click(favoriteBtnParent);
 
     render.unmount();
-    await act(() => renderWithRouter(<App />, { initialEntries: ['/meals/52772/in-progress'] }));
+    await act(() => renderWithRouter(<App />, { initialEntries: [recipeInProgressPath] }));
 
-    let favoriteBtn = screen.getByTestId('favorite-btn');
-    expect(favoriteBtn.getAttribute('src')).toEqual('/src/images/whiteHeartIcon.svg');
+    const favoriteBtn = screen.getByTestId(favoriteBtnId);
+    expect(favoriteBtn.getAttribute('src')).toEqual(whiteHeartIconPath);
   });
 
   test('Deve riscar os igredientes checados', async () => {
@@ -80,18 +82,18 @@ describe('Testes tela de receitas em progresso', async () => {
     await userEvent.click(listitems[0].children[0]);
 
     render.unmount();
-    await act(() => renderWithRouter(<App />, { initialEntries: ['/meals/52772/in-progress'] }));
+    await act(() => renderWithRouter(<App />, { initialEntries: [recipeInProgressPath] }));
 
     listitems = screen.getAllByRole('listitem');
     expect((listitems[0].children[0] as HTMLElement).style.textDecoration).toEqual('line-through solid rgb(0, 0, 0)');
   });
 
-  test('Deve redirecionar para a página \'/done-recipes\' quando o botão \'Finish recipe\' for clicado', async () => {   
-    let listitems = screen.getAllByRole('listitem');
+  test('Deve redirecionar para a página \'/done-recipes\' quando o botão \'Finish recipe\' for clicado', async () => {
+    const listitems = screen.getAllByRole('listitem');
 
     for (let i = 0; i < listitems.length; i++) {
       const listitem = listitems[i];
-      await userEvent.click(listitem.children[0]);
+      userEvent.click(listitem.children[0]);
     }
 
     const finishRecipeBbtn = screen.getByTestId('finish-recipe-btn');
