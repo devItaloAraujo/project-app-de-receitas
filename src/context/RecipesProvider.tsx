93ddec991c8ee5fcac5652e02630cc739c047758
Italo Araujo <italojsouza@gmail.com>
2023-08-30 09:18:01 -0300

    Lint

--- src/context/RecipesProvider.tsx
+++ src/context/RecipesProvider.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import RecipesContext from './RecipesContext';
 import { DataType } from '../types';
 
